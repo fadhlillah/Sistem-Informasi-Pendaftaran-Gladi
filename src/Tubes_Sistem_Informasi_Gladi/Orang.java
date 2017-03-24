c1b62e511e0477d3659c83d9f64912421f1caccd
Fadhlillah <palelu.3912@gmail.com>
2017-03-24 10:06:20 +0700

    Add files via upload
    fadhlillah

--- src/Tubes_Sistem_Informasi_Gladi/Orang.java
+++ src/Tubes_Sistem_Informasi_Gladi/Orang.java
@@ -1,22 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Tubes_Sistem_Informasi_Gladi;
+package tubes;
 
-import java.sql.Date;
-
-/**
- *
- * @author chand
- */
-public class Orang {
+public abstract class Orang {
     private String nama;
-    private Date tglLahir;
-    private String tmptLahir;
-    private String alamat;
-    private int noHp;
-    private String email;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    
     
 }
